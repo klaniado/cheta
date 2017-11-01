b625e2672ac00582286528a9bccec3374f1128ad
Gonzalink <31114523+Gonzalink@users.noreply.github.com>
2017-11-01 16:55:12 -0300

    Add files via upload

---
+++ cheta/categorias.php
@@ -0,0 +1,45 @@
+<?php
+  $title="Categorias";
+  require_once("head.php");
+?>
+
+  <body>
+    <?php
+    include("./header.php");
+    ?>
+    <section>
+
+<article class="categ">
+  <br><br>
+  <a href="mac.php" class="titles"><h2 class="titles">Mac Book</h2></a>
+  <a href="mac.php"> <img class="img-mac"src="images/productos/MacBookPro.png" alt=""> </a>
+  <br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+
+<article class="categ">
+
+  <a href="ipad.php" class="titles"><h2 class="titles">Ipad</h2></a>
+  <a href="ipad.php"> <img class="img-ipad"src="images/productos/IpadPro.png" alt=""> </a>
+  <br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</article>
+
+<article class="categ">
+
+  <a href="iphone.php" class="titles"><h2 class="titles">Iphone</h2></a>
+  <a href="iphone.php"> <img class="img-iphone" src="images/productos/Iphone7+.png" alt=""> </a>
+  <br>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+</article>
+
+    </section>
+
+    <?php
+    include("./footer.php");
+     ?>
+  </body>
+</html>
---
+++ cheta/ipad.php
@@ -0,0 +1,40 @@
+<?php
+  $title="Ipads";
+  require_once("head.php");
+?>
+
+  <body>
+    <div class="container">
+      <?php include "header.php" ?><br><br><br>
+    <h1>Ipads</h1>
+    <section>
+
+      <article class="producto">
+        <h2>Ipad Pro</h2>
+      <a href="#"> <img src="images/productos/IpadPro.png" alt="pdto 02"> </a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      </article>
+    <article class="producto">
+      <h2>Ipad Air</h2>
+      <a href="#"> <img src="images/productos/IpadAir.png" alt="pdto 05"> </a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      </article>
+  <article class="producto">
+    <h2>Ipad Mini 4</h2>
+      <a href="#"> <img src="images/productos/Ipad4Mini.png" alt="pdto 08"> </a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      </article>
+  <article class="producto">
+    <h2>Ipad 4</h2>
+      <a href="#"> <img src="images/productos/Ipad4.png" alt="pdto 11"> </a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+      </article>
+
+    </section>
+
+    <?php
+    include("./footer.php");
+     ?>
+   </div>
+  </body>
+</html>
